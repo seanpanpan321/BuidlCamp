d2db90d83db4a30328fb642da617bc5b62865059
Fel-x-ma <90840938+Fel-x-ma@users.noreply.github.com>
2022-12-02 14:26:44 +0800

    Update solona.ts

--- app/src/test/solona.ts
+++ app/src/test/solona.ts
@@ -4,3 +4,21 @@ export function testMint(){
     const walletPubKey = '1234567890'
     solona.mint(walletPubKey);
 }
+
+
+
+/***** Function *****/
+//Get Salona Public Key 
+
+/***** Function *****/
+//Pay function
+
+/***** Function *****/
+//Mint receipt after paid confirm
+
+/***** Function *****/
+//Export NFT in image and JSON
+
+
+
+
